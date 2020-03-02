bc77eabb2a61868b81da1cc8c119ac69a88289f9
huuht <huahuu1@gmail.com>
2020-03-02 10:43:09 +0700

    change

--- ProjectHKII-Website/dist/js/owl-carousel.js
+++ ProjectHKII-Website/dist/js/owl-carousel.js
@@ -1,16 +1,26 @@
 "use strict";
 
-$('.owl-carousel').owlCarousel({
-  margin: 10,
-  loop: true,
-  responsiveClass: true,
-  dots: false,
-  autoplay: false,
-  nav: true,
-  navText: ["<i class='fa fa-chevron-left prev-next-button'></i>", "<i class='fa fa-chevron-right prev-next-button'></i>"],
-  responsive: {
-    0: {
-      items: 1
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    margin: 10,
+    loop: true,
+    responsiveClass: true,
+    dots: false,
+    autoplay: true,
+    lazyLoad: true,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left prev-next-button'></i>", "<i class='fa fa-chevron-right prev-next-button'></i>"],
+    responsive: {
+      0: {
+        items: 1
+      }
     }
-  }
-});
+  });
+}); // $(document).ready(function(){
+//     $('.owl-carousel').slick({
+//         infinite: true,
+//         slidesToShow: 1,
+//         arrows: true,
+//         dots: false
+//     });
+// });
